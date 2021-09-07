e1c621b1292e3f5ea599ebb526e7084e40f4d19d
Ruben Yebran <RubenYebran@gmail.com>
2021-09-07 13:07:46 -0400

    Changes in gradle and ViewModel

--- app/src/main/java/com/example/examplemvvm/presentation/PokemonViewModel.kt
+++ app/src/main/java/com/example/examplemvvm/presentation/PokemonViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.examplemvvm.presentation
 
+import android.content.res.Resources
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.examplemvvm.core.Resource
+import com.example.examplemvvm.data.model.Pokemon
 import com.example.examplemvvm.repository.PokemonRepository
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+
 
 class PokemonViewModel(private val repo: PokemonRepository) : ViewModel() {
 
-    fun fetchPokemons(limit: String, offset:String) = liveData(Dispatchers.IO) {
+    fun fetchPokemons(limit: String, offset: String) = liveData(Dispatchers.IO) {
         emit(Resource.Loading())
         try {
-            emit(Resource.Success(repo.getPokemons(limit,offset)))
+            emit(Resource.Success(repo.getPokemons(limit, offset)))
         } catch (e: Exception) {
             emit(Resource.Failure(e))
         }
